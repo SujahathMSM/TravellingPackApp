e448cc11d1476ce43e90eab361568ce8a0d780cd
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-20 15:16:18 +0530

    Added the UI

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
+    </div>
+  )
+}
+
+function Logo(){
+  return <h1> 🌴Far Away 👜</h1>
+}
+
+function Form(){
+  return <div className='add-form'>
+    <h3>What do you need for your 😍 trip?</h3>
+  </div>
+}
+
+function PackingList(){
+  return <div className='list'>LIST</div>
+}
+
+function Stats(){
+  return (
+    <footer className='stats'>
+      <em>🛍️ You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
+
+export default App
