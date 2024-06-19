088544b16b64a60c75f83fba21c4bb356305ac43
sujahathmsm <sujahathmhmd@gmail.com>
2024-06-19 21:19:42 +0530

    Rendered Items from List

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,61 @@
-import React from 'react'
+import React from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 const App = () => {
   return (
-    <div className='app'>
-      <Logo/>
-      <Form/>
-      <PackingList/>
-      <Stats/>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-  )
+  );
+};
+
+function Logo() {
+  return <h1> 🌴Far Away 👜</h1>;
 }
 
-function Logo(){
-  return <h1> 🌴Far Away 👜</h1>
-}
-
-function Form(){
-  return <div className='add-form'>
-    <h3>What do you need for your 😍 trip?</h3>
-  </div>
-}
-
-function PackingList(){
-  return <div className='list'>LIST</div>
-}
-
-function Stats(){
+function Form() {
   return (
-    <footer className='stats'>
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
       <em>🛍️ You have X items on your list, and you already packed X (X%)</em>
     </footer>
-  )
+  );
 }
 
-export default App
+export default App;
